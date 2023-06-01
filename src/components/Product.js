84e2487b9a48c49021d3b1656d6593deb7dcb271
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-01 07:10:14 -0300

    badge ok

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,26 @@
 import React from 'react';
+// link
+import {Link} from 'react-router-dom'
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({product}) => {
+    console.log(product)
+    return (
+        <Link>
+            <div className='grad w-full h-[362px] rounded-[8px] overflow-hidden relative group'>
+                {/*badge*/}
+                <div className='absolute bg-accent text-primary text-[12px] font-extrabold uppercase top-4 right-4 px-2
+                 rounded-full z-10' >new</div>
+
+                {/*image*/}
+                <div className='w-full h-[200px] flex items-center justify-center relative transition-all'>
+                    <img
+                    className='w-[160px] h-[160px] group-hover:scale-90'
+                        src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
+                        alt=''/>
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default Product;
