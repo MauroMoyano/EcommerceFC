2ea12dcef4be516e3bf7d1925b13bb2540847f90
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-05 03:51:57 -0300

    Hero in procces!

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,33 @@
 import React from 'react';
+// import components
+import CategoryNav from "./CategoryNav";
+import MainSlider from "./MainSlider";
+// images
+import PromoImg1 from '../img/promo_img1.png'
+import PromoImg2 from '../img/promo_img2.png'
 
 const Hero = () => {
-  return <div>Hero</div>;
+    return <section className='mb-[30px] pt-36 lg:pt-0'>
+        <div className='container mx-auto'>
+            <div className='flex flex-col gap-y-[30px] xl:flex-row xl:gap-x-[30px] lg:max-w-[734px] mx-auto'>
+                {/*  sidebar  */}
+                <div>
+                    <CategoryNav/>
+                </div>
+                {/*  main sidebar  */}
+                <div className='w-full max-w-lg bg-pink-50'>
+                    <MainSlider />
+                </div>
+                {/*  promo images   */}
+                <div>
+                    {/*    promo 1   */}
+                    <div>promo 1</div>
+                    {/*    promo 2   */}
+                    <div>promo 2</div>
+                </div>
+            </div>
+        </div>
+    </section>;
 };
 
 export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,15 @@
 import React from 'react';
-//useFetch
-import useFetch from '../hooks/useFetch.js'
 // import components
-import ProductSlider from "../components/ProductSlider";
+import LatestProducts from "../components/LatestProducts";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  //get new products
-  const { data } = useFetch('products?populate=*&filters[isNew]=true')
-    console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'+data)
+
   return (
-      <div className='mb-16'>
-          <div className='container mx-auto' >
-              <h2 className='h2 mb-6 text-center xl:text-left'>Latest Products</h2>
-          </div>
-        <ProductSlider data={data} />
-      </div>
+      <section>
+          <Hero />
+          <LatestProducts />
+      </section>
   );
 };
 
