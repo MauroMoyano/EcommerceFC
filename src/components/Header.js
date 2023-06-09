f036e673d74f467d11c87ff8ccc5d8038b3b063e
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-09 07:30:19 -0300

    Header ok -> CategoryNavMobile.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 // images
 import Logo from '../img/logo.png';
 // icons
@@ -10,18 +10,21 @@ import {Link} from 'react-router-dom';
 import SearchForm from "./SearchForm";
 import CategoryNavMobile from "./CategoryNavMobile";
 import Cart from "./Cart";
+import {CartContext} from "../context/CartContext";
 
 const Header = () => {
-    return <header>
+    const { isOpen, setIsOpen} = useContext(CartContext);
+    const [catNavMobile, setCatNavMobile] = useState(false)
+    return <header className='bg-primary py-6 fixed w-full top-0 z-40 xl:relative xl:mb-[30px]'>
         <div className='container mx-auto'>
-            <div>
+            <div className='flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0'>
             {/*    menu     */}
-                <div>
+                <div className='text-3xl xl:hidden cursor-pointer'>
                     <FiMenu/>
                 </div>
             {/*  category nav mobile  */}
-                <div>
-                    <CategoryNavMobile/>
+                <div className={`${catNavMobile ? 'left-0' : '-left-full'} fixed top-0 bottom-0 z-30 w-full h-screen transition-all duration-200`}>
+                    <CategoryNavMobile setCatNavMobile={setCatNavMobile}/>
                 </div>
             {/*    logo     */}
                 <Link to={'/'}>
@@ -36,13 +39,13 @@ const Header = () => {
                     {/*     phone      */}
                     <div>Need help? 123 456 789</div>
                     {/*     Cart icon      */}
-                    <div className='relative cursor-pointer'>
+                    <div onClick={()=> setIsOpen(!isOpen)} className='relative cursor-pointer'>
                         <SlBag className='text-2xl'/>
                     {/*     amount       */}
                         <div>2</div>
                     </div>
                 {/*    cart     */}
-                    <div className='bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 max-w-[500px] transition-all duration-300'>
+                    <div className={`${isOpen ? 'right-0' : '-right-full'} bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 max-w-[500px] transition-all duration-300`}>
                         <Cart />
                     </div>
                 </div>
