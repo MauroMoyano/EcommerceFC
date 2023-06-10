583f70e186cdf976a9c2367934383e1c9dd86f5a
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-10 05:06:57 -0300

    category nav ok

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -29,20 +29,21 @@ const SearchForm = () => {
             setSearchTerm('')
         } else {
             // if input is empty set animation to true
-            console.log('entro')
             setIsAnimating(true);
         }
     }
 
     return (
-        <form onClick={handleSubmit} className={`${isAnimating ? 'animate-shake' : 'animate-none'} w-full relative`}>
+        <form className={`${isAnimating ? 'animate-shake' : 'animate-none'} w-full relative`}>
             <input
                 onChange={handleSearchInput}
                 className='input'
                 type='text'
                 placeholder='Search for product...'
             />
-            <button className='btn btn-accent absolute top-0 right-0 rounded-tl-none rounded-bl-none'>
+            <button className='btn btn-accent absolute top-0 right-0 rounded-tl-none rounded-bl-none'
+                    onClick={handleSubmit}
+            >
                 <FiSearch className='text-xl'/>
             </button>
         </form>
