3546c2f7897b5a22338db2699520e483baa73fa5
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-11 20:57:37 -0300

    Product Details and related products ok

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -10,13 +10,13 @@ import {CartContext} from "../context/CartContext";
 
 const ProductDetails = () => {
     const {id} = useParams();
-    console.log(id)
     // get product data base on the id
     const {data} = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
     if (!data) {
         return <div className='container mx-auto '>LOADING...</div>
     }
-
+    // category title
+    const categoryTitle = data[0].attributes.categories.data[0].attributes.title;
     return (
         <div className='mb-16 pt-44 lg:pt-[30px] xl:pt-0'>
             <div className='container mx-auto'>
@@ -50,7 +50,7 @@ const ProductDetails = () => {
                     </div>
                 </div>
                 {/*    related products     */}
-                <RelatedProducts/>
+                <RelatedProducts categoryTitle={categoryTitle} />
             </div>
         </div>
     );
